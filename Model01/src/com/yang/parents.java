90941ca86303b4433f1cbbbcc1b53a7ccd11c66a
yangyueyue <yangyueyueaa@163.com>
2020-11-15 13:24:41 +0800

    增加了parent类

---
+++ Model01/src/com/yang/parents.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/15 - 13:23
+ */
+public class parents {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbb");
+    }
+}
