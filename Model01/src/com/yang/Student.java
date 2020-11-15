654c6430576a5ccdfadbd837659e58fa752c2e8e
yangyunyue <61929308+yangyunyue@users.noreply.github.com>
2020-11-15 13:19:38 +0800

    Update Student.java

--- Model01/src/com/yang/Student.java
+++ Model01/src/com/yang/Student.java
@@ -10,5 +10,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("good good study");
         System.out.println("day day up");
+          System.out.println("我就是一个废物");
+        
     }
 }
